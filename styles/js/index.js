3299368ee2bcc54736d6921269254801adfe4174
Dadouminal <86045435+Dadouminal@users.noreply.github.com>
2022-09-25 09:03:41 +0000

    Add files via upload

---
+++ styles/js/index.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach((el) => {
+    el.addEventListener("click", () => {
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle("is-active");
+      $target.classList.toggle("is-active");
+    });
+  });
+});
+
+// faire une navbar fixed
+
+const nav = document.querySelector(".navbar");
+
+// console.log(nav);
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 120) {
+    nav.classList.add("is-fixed-top");
+  } else {
+    nav.style.top = "0";
+  }
+});
